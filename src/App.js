58dde5042e4871e66db5290f53f1d222284a9acb
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-02-20 12:17:41 -0800

    Added index and app files to render the React app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+  } from 'react-router-dom'
+import Header from './components/Header'
+
+const App = () => {
+    return (
+    <Router>
+        <Header />
+    </Router>
+    )
+}
+
+export default App
