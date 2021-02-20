ee59076db2178b8f11cef0c571a178035153bfda
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-02-20 12:44:29 -0800

    Synced up front and back end for complete functionality

--- src/App.js
+++ src/App.js
@@ -6,14 +6,21 @@ import {
   } from 'react-router-dom'
 import Header from './components/Header'
 import HomePage from './components/HomePage'
+import Gallery from './components/Gallery'
 
 const App = () => {
     return (
     <Router>
     <Switch>
+    <Route exact path='/'>
         <Header />
         <HomePage />
-        </Switch>
+    </Route>
+    <Route exact path='/gallery'>
+        <Header />
+        <Gallery />
+    </Route>
+    </Switch>
     </Router>
     )
 }
