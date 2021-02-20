edcc44e116ffaa889a582c597166ecc90d7b8892
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-02-20 12:07:13 -0800

    Added a new DB user and obtained auth

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,8 +1,11 @@
-require('dotenv').confit()
+require('dotenv').config()
 const mongoose = require('mongoose');
 
-const DB_URI = `mongodb+srv://dbUser:${process.env.DB_PASSWORD}@images.bi4kf.mongodb.net/image_gallery?retryWrites=true&w=majority`
+const DB_URI = `mongodb+srv://iaUser:${process.env.DB_PASSWORD}@images.bi4kf.mongodb.net/images?retryWrites=true&w=majority`
+
 mongoose.connect(DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-});
+})
+  .then(response => console.log('DB Connected'))
+  .catch(error => console.log(error));
