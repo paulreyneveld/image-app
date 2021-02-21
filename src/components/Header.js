8e30bcb162c290a57b65449cb8db86908a04f003
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-02-21 15:36:25 -0800

    CSS changes for the background and links

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => (
   <header>
-    <h1>Photo Gallery App</h1>
+    <h1 style={{ marginTop: 20 }}>Photo Gallery App</h1>
     <div className="links">
       <NavLink to="/" className="link">
         Home
